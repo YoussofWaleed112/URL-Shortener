f4492f25fe0278adf7fc3ba6d8515aaa96a5c918
Youssof_Waleed112 <168733163+YoussofWaleed112@users.noreply.github.com>
2026-04-26 20:13:14 +0300

    Delete Downloads/url_shortener_project/url_shortener_project directory

--- Downloads/url_shortener_project/url_shortener_project/header_files/validator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Validator {
-    bool isValidUrl(const std::string& url);
-    std::string extractHost(const std::string& url);    // "github.com"
-    std::string extractPath(const std::string& url);    // "/user/project/issues/154"
-}
